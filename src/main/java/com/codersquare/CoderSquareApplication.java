1199240e6d0ea3c1febea984621355046babad12
Hosam Youssef <hosamyoussef303@gmail.com>
2023-12-13 23:22:47 +0200

    init commit

---
+++ src/main/java/com/codersquare/CoderSquareApplication.java
@@ -0,0 +1,13 @@
+package com.codersquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codersquare/CoderSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
