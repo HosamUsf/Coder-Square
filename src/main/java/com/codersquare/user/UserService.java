de8d811a6928c53952e21fa16e774c041fbd319b
Hosam Youssef <hosamyoussef303@gmail.com>
2023-12-26 19:02:37 +0200

    refactor user service

--- src/main/java/com/codersquare/user/UserService.java
+++ src/main/java/com/codersquare/user/UserService.java
@@ -11,40 +11,51 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+/**
+ * Service class for managing users.
+ */
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final PostRepository postRepository;
     private final UserDTOMapper userDTOMapper;
 
-
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return List of UserDTO representing all users
+     */
     public List<UserDTO> getAllUsers() {
         return userRepository.findAll().stream()
                 .map(userDTOMapper).toList();
-
     }
 
-
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param id User ID
+     * @return UserDTO representing the user
+     */
     public UserDTO getUserById(Long id) {
         return userRepository.findById(id)
                 .map(userDTOMapper)
                 .orElseThrow(() -> new EntityNotFoundException("User Doesn't Exist"));
     }
 
+    /**
+     * Deletes a user based on the provided username.
+     *
+     * @param userName Username of the user to be deleted
+     * @return ResponseEntity with the status and response body
+     */
     @Transactional
     public ResponseEntity<DeleteEntityResponse> deleteUser(String userName) {
         try {
             checkIfUsernameExists(userName);
 
-
-            User user = userRepository.findUserByUserName(userName).orElse(null);
-            if (user != null) {
-                List<Post> userPosts = postRepository.findAllByUserId(user.getUserId());
-                postRepository.deleteAll(userPosts);
-            }
             userRepository.deleteUserByUserName(userName);
             return ResponseEntity.status(HttpStatus.OK).
                     body(DeleteEntityResponse.success("User", "User Name", userName));
@@ -54,12 +65,15 @@ public class UserService {
         }
     }
 
-
+    /**
+     * Checks if a user exists.
+     *
+     * @param username The username of the user.
+     * @throws EntityNotFoundException if the user does not exist.
+     */
     private void checkIfUsernameExists(String username) {
         if (!userRepository.existsByUserName(username)) {
             throw new EntityNotFoundException("User with user name " + username + " Not Found");
         }
     }
-
-
 }
